50d76a39f530e1e8f20da4ed62ec1c9d1bc8cff4
showvike <showvike@gmail.com>
2023-02-12 22:48:58 +0600

    bank work ongoing again

--- js/deposit.js
+++ js/deposit.js
@@ -1,7 +1,12 @@
 document.getElementById("btn-deposit").addEventListener("click", function () {
     const depositField = document.getElementById("deposit-amount");
-    const depositValue = depositField.value;
+    const depositValue = parseFloat(depositField.value);
 
     const deposit = document.getElementById("deposit");
-    deposit.innerHTML = depositValue;
+    deposit.innerHTML = parseFloat(deposit.innerHTML) + depositValue;
+
+    const balance = document.getElementById("balance");
+    balance.innerHTML = parseFloat(balance.innerHTML) + depositValue;
+
+    depositField.value = "";
 });
