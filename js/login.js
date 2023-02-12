22dbda3eb4152c09a145ab365567572abb420a26
showvike <showvike@gmail.com>
2023-02-12 17:42:48 +0600

    bank work started

--- js/login.js
+++ js/login.js
@@ -5,6 +5,6 @@ document.getElementById("btn-submit").addEventListener("click", function () {
     const passwordForm = document.getElementById("user-password");
     const password = passwordForm.value;
 
-    if (email === "sontan@baap.com" && password === "secret") console.log("valid user");
-    else console.log("invalid user");
+    if (email === "sontan@baap.com" && password === "secret") window.location.href = "./bank.html";
+    else alert("vuilla gesos!!! tore tejjo sontan korlam.");
 });
